e479ed4adc4c19bff6e0f6b65b1dbb7cd906d1e9
Sergey Mysov <s.mysov@list.ru>
2021-02-26 23:58:45 +0400

    feat: added first test for function date

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	clearMocks: true, //Очистка моков при каждом запуске
+	collectCoverageFrom: ['src/**/*.js'], //Точка сбора информации по покрытию тестами
+	coverageDirectory: 'coverage', //Место хранения отчетов
+	moduleFileExtensions: ['js'], //Какие файлы тестируются
+	testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'], //Где находятся тесты
+	testPathIgnorePatterns: ['\\\\node_modules\\\\'],
+	transformIgnorePatterns: ['<rootDir>/node_modules/'],
+	transform: {
+		'.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+		'^.+\\.(js|jsx)?$': 'babel-jest',
+	},
+	verbose: false, //Отключения подробной информации о тесте
+};
+
